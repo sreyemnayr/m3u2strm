67c025e9bc7da694237ede67262c230f19f046f3
Ryan Meyers <ryan@forkhunger.art>
2025-01-15 16:38:01 -0600

    create crontab debug

--- create_crontab.sh
+++ create_crontab.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
 cd /app
+echo "CRONTAB_SCHEDULE: $CRONTAB_SCHEDULE"
 echo "$CRONTAB_SCHEDULE export PYTHONPATH=/app && python /app/main.py >> /app/logs/cron.log 2>&1" > crontab
 crontab crontab
